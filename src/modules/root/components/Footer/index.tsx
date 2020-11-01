e29513e4ad6bb6c5f52e6c2dd2d843492e352dad
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-11-01 17:00:08 +0700

    Fix Footer UI

--- src/modules/root/components/Footer/index.tsx
+++ src/modules/root/components/Footer/index.tsx
@@ -3,10 +3,12 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
 function Copyright() {
   return (
-    <Typography variant="body2" color="inherit" align="center">
+    <Typography variant="body2" color="inherit" align="left">
       {"Copyright © "}
       <Link color="secondary" href="https://material-ui.com/">
         Office of the Civil Service Commission (OCSC)
@@ -41,10 +43,34 @@ export default function Footer() {
   return (
     <footer className={classes.footer}>
       <Container maxWidth="lg">
-        <Typography variant="body1" color="inherit" align="center">
-          สำนักงานคณะกรรมการข้าราชการพลเรือน (สำนักงาน ก.พ.)
-        </Typography>
-        <Copyright />
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="flex-start"
+        >
+          <Box>
+            <Typography variant="h6" color="inherit" align="left">
+              สำนักงานคณะกรรมการข้าราชการพลเรือน (สำนักงาน ก.พ.)
+            </Typography>
+            <Copyright />
+          </Box>
+          <Box>
+            <Typography variant="body2" color="inherit" align="right">
+              47/111 หมู่ 4 ถนนติวานนท์ ตำบลตลาดขวัญ อำเภอเมือง จังหวัดนนทบุรี
+              11000
+            </Typography>
+            <Typography variant="body2" color="inherit" align="right">
+              E-mail:{" "}
+              <Link href="mailto:ocsc.hrd@gmail.com" color="secondary">
+                ocsc.hrd@gmail.com
+              </Link>
+            </Typography>
+            <Typography variant="body2" color="inherit" align="right">
+              โทร. 02-547-1795 , 02-547-1807 (ภายในเวลาราชการ)
+            </Typography>
+          </Box>
+        </Grid>
       </Container>
     </footer>
   );
