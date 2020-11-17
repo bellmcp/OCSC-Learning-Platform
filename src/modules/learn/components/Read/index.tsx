4654f9b0a16d12778a29b78ee80540a20f8bf4a3
Wutipat Khamnuansin <bellmcp@gmail.com>
2020-11-18 03:22:17 +0700

    Update lecture UI

--- src/modules/learn/components/Read/index.tsx
+++ src/modules/learn/components/Read/index.tsx
@@ -3,6 +3,10 @@ import { Document, Page, pdfjs } from "react-pdf";
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Link from "@material-ui/core/Link";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 interface onDocumentLoadSuccessProps {
@@ -37,15 +41,31 @@ export default function Read() {
     <React.Fragment>
       <Container maxWidth="md">
         <Box my={2}>
-          <Typography
-            variant="h6"
-            color="initial"
-            gutterBottom
-            style={{ fontSize: "1.4rem", fontWeight: 600 }}
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
           >
-            เอกสาร: หลักธรรมเบื้องต้น
-          </Typography>
+            <Typography
+              variant="h6"
+              color="initial"
+              gutterBottom
+              style={{ fontSize: "1.4rem", fontWeight: 600 }}
+            >
+              เอกสาร: หลักธรรมเบื้องต้น
+            </Typography>
+            <Link
+              href="https://ocsc-learning-platform.herokuapp.com/sample.pdf"
+              target="_blank"
+            >
+              <Button variant="outlined" color="default">
+                ดาวน์โหลดไฟล์
+              </Button>
+            </Link>
+          </Grid>
         </Box>
+        <Divider />
         <Box my={4}>
           <PDFContent />
         </Box>
--- src/modules/learn/components/Video/index.tsx
+++ src/modules/learn/components/Video/index.tsx
@@ -1,5 +1,7 @@
 import Box from "@material-ui/core/Box";
 import Container from "@material-ui/core/Container";
+import Divider from "@material-ui/core/Divider";
+import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
@@ -15,45 +17,54 @@ export default function Video() {
     event: React.MouseEvent<HTMLElement>,
     newSource: string | null
   ) => {
-    setSource(newSource);
+    if (newSource !== null) {
+      setSource(newSource);
+    }
   };
 
   return (
     <React.Fragment>
       <Container maxWidth="md">
         <Box my={2}>
-          <Typography
-            variant="h6"
-            color="initial"
-            gutterBottom
-            style={{ fontSize: "1.4rem", fontWeight: 600 }}
+          <Grid
+            container
+            direction="row"
+            justify="space-between"
+            alignItems="center"
           >
-            วิดีโอ: พระพุทธศาสนา
-          </Typography>
+            <Typography
+              variant="h6"
+              color="initial"
+              gutterBottom
+              style={{ fontSize: "1.4rem", fontWeight: 600 }}
+            >
+              วิดีโอ: พระพุทธศาสนา
+            </Typography>
+            <ToggleButtonGroup
+              value={source}
+              exclusive
+              onChange={handleSource}
+              aria-label="สลับลิงก์"
+              size="small"
+            >
+              <ToggleButton
+                value="https://ocsc-learning-platform.herokuapp.com/video.mp4"
+                aria-label="ลิงก์หลัก"
+              >
+                ลิงก์หลัก
+              </ToggleButton>
+              <ToggleButton
+                value="https://www.youtube.com/watch?v=DJX9fCdgMsg"
+                aria-label="ลิงก์สำรอง"
+              >
+                สำรอง
+              </ToggleButton>
+            </ToggleButtonGroup>
+          </Grid>
         </Box>
+        <Divider />
         <Box my={4}>
           <Player url={source} />
-        </Box>
-        <Box my={4}>
-          <ToggleButtonGroup
-            value={source}
-            exclusive
-            onChange={handleSource}
-            aria-label="สลับลิงก์"
-          >
-            <ToggleButton
-              value="https://ocsc-learning-platform.herokuapp.com/video.mp4"
-              aria-label="ลิงก์หลัก"
-            >
-              ลิงก์หลัก
-            </ToggleButton>
-            <ToggleButton
-              value="https://www.youtube.com/watch?v=DJX9fCdgMsg"
-              aria-label="ลิงก์สำรอง"
-            >
-              ลิงก์สำรอง
-            </ToggleButton>
-          </ToggleButtonGroup>
         </Box>
       </Container>
     </React.Fragment>
