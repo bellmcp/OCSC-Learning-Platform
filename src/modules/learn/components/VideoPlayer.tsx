2021700bfa196f97590c70d66ebdd9b97997ef46
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-02-28 21:07:43 +0700

    Fix react player blue border

--- src/modules/learn/components/VideoPlayer.tsx
+++ src/modules/learn/components/VideoPlayer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactPlayer from "react-player";
+import styled from "styled-components";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -19,19 +20,27 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
+const ReactPlayerWrapper = styled.div`
+  video {
+    outline: 0;
+  }
+`;
+
 export default function VideoPlayer({ url }: any) {
   const classes = useStyles();
 
   return (
     <div className={classes.playerWrapper}>
-      <ReactPlayer
-        url={url}
-        controls
-        playing
-        width="100%"
-        height="100%"
-        className={classes.player}
-      />
+      <ReactPlayerWrapper>
+        <ReactPlayer
+          url={url}
+          controls
+          playing
+          width="100%"
+          height="100%"
+          className={classes.player}
+        />
+      </ReactPlayerWrapper>
     </div>
   );
 }
