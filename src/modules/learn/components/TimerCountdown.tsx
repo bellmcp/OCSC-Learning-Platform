6d7b4ab2ea3a04a32e553092a9cacc688cd90cfd
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-03-17 00:50:23 +0700

    Implement timer countdown text

--- src/modules/learn/components/TimerCountdown.tsx
+++ src/modules/learn/components/TimerCountdown.tsx
@@ -1,5 +1,6 @@
 //@ts-nocheck
 import React, { useState, useEffect } from "react";
+import DayJS from "react-dayjs";
 import { useDispatch } from "react-redux";
 import {
   Grid,
@@ -25,6 +26,38 @@ export default function TimerCountdown({}: any) {
     return () => {
       clearInterval(round);
     };
+  }, []);
+
+  //COUNTDOWN TIMER
+  const initialMinute = 3;
+  const initialSeconds = 0;
+  const [minutes, setMinutes] = useState(initialMinute);
+  const [seconds, setSeconds] = useState(initialSeconds);
+
+  useEffect(() => {
+    let myInterval = setInterval(() => {
+      if (seconds > 0) {
+        setSeconds(seconds - 1);
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(myInterval);
+        } else {
+          setMinutes(minutes - 1);
+          setSeconds(59);
+        }
+      }
+    }, 1000);
+    return () => {
+      clearInterval(myInterval);
+    };
+  }, [minutes, seconds]);
+
+  //ENDTIME
+  const [currentTime, setCurrentTime] = useState();
+
+  useEffect(() => {
+    setCurrentTime(new Date().toISOString());
   }, []);
 
   function CircularProgressWithLabel(
@@ -59,11 +92,17 @@ export default function TimerCountdown({}: any) {
           <Hidden only={["xs"]}>
             <b>คงเหลือ </b>
           </Hidden>
-          0 นาที 0 วินาที
+          {minutes === 0 && seconds === 0
+            ? "หมดเวลา"
+            : `${minutes} นาที ${seconds} วินาที`}
         </Typography>
       </Grid>
       <Grid item style={{ width: "100px" }}>
-        สิ้นสุด 00:00 น.
+        <b>สิ้นสุด </b>
+        <DayJS format="HH:mm" add={{ minutes: 3 }}>
+          {currentTime}
+        </DayJS>{" "}
+        น.
       </Grid>
     </Grid>
   );
