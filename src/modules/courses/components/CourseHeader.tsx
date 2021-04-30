8852f7f683bd540b94963f43722f1878c522e13c
Wutipat Khamnuansin <bellmcp@gmail.com>
2021-04-30 20:05:26 +0700

    Update course and curriculum header design

--- src/modules/courses/components/CourseHeader.tsx
+++ src/modules/courses/components/CourseHeader.tsx
@@ -5,7 +5,13 @@ import {
   Theme,
   useTheme,
 } from "@material-ui/core/styles";
-import { useMediaQuery, Container, Typography, Grid } from "@material-ui/core";
+import {
+  useMediaQuery,
+  Container,
+  Typography,
+  Grid,
+  Toolbar,
+} from "@material-ui/core";
 import { FiberManualRecord as CourseGenreIcon } from "@material-ui/icons";
 import categoryColor from "utils/categoryColor";
 
@@ -16,8 +22,8 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundSize: "cover",
       backgroundPosition: "center center",
       minHeight: "370px",
-      padding: theme.spacing(16, 0, 8),
-      textShadow: "0px 3px 3px rgba(0, 0, 0, 0.4)",
+      padding: theme.spacing(14, 0, 8),
+      textShadow: "0px 3px 3px rgba(0, 0, 0, 0.2)",
     },
     subtitle: {
       marginTop: theme.spacing(4),
@@ -37,65 +43,68 @@ export default function CourseHeader({
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
-    <div
-      className={classes.header}
-      style={{
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.35), rgba(0,0,0,0.35)), url(${imageUrl})`,
-        backgroundSize: "cover",
-        backgroundBlendMode: "darken",
-      }}
-    >
-      <div style={{ position: "relative" }}>
-        <Container maxWidth="lg">
-          <Grid
-            container
-            direction="column"
-            justify="space-between"
-            alignItems={matches ? "flex-start" : "center"}
-          >
-            <Typography
-              component="h1"
-              variant={matches ? "h3" : "h4"}
-              align="center"
-              color="inherit"
-              gutterBottom
-            >
-              {title}
-            </Typography>
-            <Typography
-              component="h2"
-              variant={matches ? "h6" : "body1"}
-              align="center"
-              color="inherit"
-              gutterBottom
-            >
-              {code}
-            </Typography>
+    <>
+      <Toolbar />
+      <div
+        className={classes.header}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.35), rgba(0,0,0,0.35)), url(${imageUrl})`,
+          backgroundSize: "cover",
+          backgroundBlendMode: "darken",
+        }}
+      >
+        <div style={{ position: "relative" }}>
+          <Container maxWidth="lg">
             <Grid
               container
-              justify={matches ? "flex-start" : "center"}
-              alignItems="baseline"
-              className={classes.subtitle}
-              spacing={1}
+              direction="column"
+              justify="space-between"
+              alignItems={matches ? "flex-start" : "center"}
             >
-              <Grid item>
-                <CourseGenreIcon
-                  style={{
-                    color: categoryColor(categoryId),
-                    fontSize: 12,
-                    marginRight: 6,
-                  }}
-                />
-              </Grid>
-              <Grid item>
-                <Typography variant="body1" align="center">
-                  {category}
-                </Typography>
+              <Typography
+                component="h1"
+                variant={matches ? "h3" : "h4"}
+                align="center"
+                color="inherit"
+                gutterBottom
+              >
+                {title}
+              </Typography>
+              <Typography
+                component="h2"
+                variant={matches ? "h6" : "body1"}
+                align="center"
+                color="inherit"
+                gutterBottom
+              >
+                {code}
+              </Typography>
+              <Grid
+                container
+                justify={matches ? "flex-start" : "center"}
+                alignItems="baseline"
+                className={classes.subtitle}
+                spacing={1}
+              >
+                <Grid item>
+                  <CourseGenreIcon
+                    style={{
+                      color: categoryColor(categoryId),
+                      fontSize: 12,
+                      marginRight: 6,
+                    }}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant="body1" align="center">
+                    {category}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Container>
+          </Container>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/modules/curriculums/components/CurriculumHeader.tsx
+++ src/modules/curriculums/components/CurriculumHeader.tsx
@@ -5,7 +5,13 @@ import {
   Theme,
   useTheme,
 } from "@material-ui/core/styles";
-import { useMediaQuery, Container, Typography, Grid } from "@material-ui/core";
+import {
+  useMediaQuery,
+  Container,
+  Typography,
+  Grid,
+  Toolbar,
+} from "@material-ui/core";
 import { amber } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -15,8 +21,8 @@ const useStyles = makeStyles((theme: Theme) =>
       backgroundSize: "cover",
       backgroundPosition: "center center",
       minHeight: "370px",
-      padding: theme.spacing(16, 0, 8),
-      textShadow: "0px 3px 3px rgba(0, 0, 0, 0.4)",
+      padding: theme.spacing(14, 0, 8),
+      textShadow: "0px 3px 3px rgba(0, 0, 0, 0.2)",
     },
     subtitle: {
       marginTop: theme.spacing(4),
@@ -30,51 +36,54 @@ export default function CurriculumHeader({ title, code, imageUrl }: any) {
   const matches = useMediaQuery(theme.breakpoints.up("sm"));
 
   return (
-    <div
-      className={classes.header}
-      style={{
-        backgroundImage: `linear-gradient(rgba(0,0,0,0.35), rgba(0,0,0,0.35)), url(${imageUrl})`,
-        backgroundSize: "cover",
-        backgroundBlendMode: "darken",
-      }}
-    >
-      <div style={{ position: "relative" }}>
-        <Container maxWidth="lg">
-          <Grid
-            container
-            direction="column"
-            justify="space-between"
-            alignItems={matches ? "flex-start" : "center"}
-          >
-            <Typography
-              component="h2"
-              variant={matches ? "h6" : "body1"}
-              align="center"
-              gutterBottom
-              style={{ color: amber[500] }}
+    <>
+      <Toolbar />
+      <div
+        className={classes.header}
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.35), rgba(0,0,0,0.35)), url(${imageUrl})`,
+          backgroundSize: "cover",
+          backgroundBlendMode: "darken",
+        }}
+      >
+        <div style={{ position: "relative" }}>
+          <Container maxWidth="lg">
+            <Grid
+              container
+              direction="column"
+              justify="space-between"
+              alignItems={matches ? "flex-start" : "center"}
             >
-              หลักสูตร
-            </Typography>
-            <Typography
-              component="h1"
-              variant={matches ? "h3" : "h4"}
-              align="center"
-              color="inherit"
-              gutterBottom
-            >
-              {title}
-            </Typography>
-            <Typography
-              component="h2"
-              variant={matches ? "h6" : "body1"}
-              align="center"
-              color="inherit"
-            >
-              {code}
-            </Typography>
-          </Grid>
-        </Container>
+              <Typography
+                component="h2"
+                variant={matches ? "h6" : "body1"}
+                align="center"
+                gutterBottom
+                style={{ color: amber[500] }}
+              >
+                หลักสูตร
+              </Typography>
+              <Typography
+                component="h1"
+                variant={matches ? "h3" : "h4"}
+                align="center"
+                color="inherit"
+                gutterBottom
+              >
+                {title}
+              </Typography>
+              <Typography
+                component="h2"
+                variant={matches ? "h6" : "body1"}
+                align="center"
+                color="inherit"
+              >
+                {code}
+              </Typography>
+            </Grid>
+          </Container>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
