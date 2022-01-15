13ccd760d367c67ac124af57bd6cf1f442b76786
Wutipat Khamnuansin <bellmcp@gmail.com>
2022-01-15 16:22:45 +0700

    Update supports API endpoint

--- src/modules/support/actions.ts
+++ src/modules/support/actions.ts
@@ -1,21 +1,25 @@
 // @ts-nocheck
-import axios from "axios";
-import { getCookie } from "utils/cookies";
-import parseJwt from "utils/parseJwt";
-import * as uiActions from "modules/ui/actions";
+import axios from 'axios';
+import { getCookie } from 'utils/cookies';
+import parseJwt from 'utils/parseJwt';
+import * as uiActions from 'modules/ui/actions';
 
-const LOAD_SUPPORT_REQUEST = "learning-platform/support/LOAD_SUPPORT_REQUEST";
-const LOAD_SUPPORT_SUCCESS = "learning-platform/support/LOAD_SUPPORT_SUCCESS";
-const LOAD_SUPPORT_FAILURE = "learning-platform/support/LOAD_SUPPORT_FAILURE";
-const SEND_SUPPORT_SUCCESS = "learning-platform/support/SEND_SUPPORT_SUCCESS";
+const LOAD_SUPPORT_REQUEST = 'learning-platform/support/LOAD_SUPPORT_REQUEST';
+const LOAD_SUPPORT_SUCCESS = 'learning-platform/support/LOAD_SUPPORT_SUCCESS';
+const LOAD_SUPPORT_FAILURE = 'learning-platform/support/LOAD_SUPPORT_FAILURE';
+const SEND_SUPPORT_SUCCESS = 'learning-platform/support/SEND_SUPPORT_SUCCESS';
 const MARK_SUPPORT_AS_READ_SUCCESS =
-  "learning-platform/support/MARK_SUPPORT_AS_READ_SUCCESS";
+  'learning-platform/support/MARK_SUPPORT_AS_READ_SUCCESS';
 
 function loadSupports() {
   return async (dispatch: any) => {
+    const token = getCookie('token');
+    const userId = parseJwt(token).unique_name;
     dispatch({ type: LOAD_SUPPORT_REQUEST });
     try {
-      var { data } = await axios.get("/Supports");
+      var { data } = await axios.get(`/Supports?userid=${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
       if (data.length === 0) {
         data = [];
       }
@@ -29,8 +33,8 @@ function loadSupports() {
       dispatch({ type: LOAD_SUPPORT_FAILURE });
       dispatch(
         uiActions.setFlashMessage(
-          `โหลดประวัติการติดต่อเจ้าหน้าที่ทั้งหมดไม่สำเร็จ เกิดข้อผิดพลาด ${err?.response?.status}`,
-          "error"
+          `โหลดประวัติการติดต่อเจ้าหน้าที่ไม่สำเร็จ เกิดข้อผิดพลาด ${err?.response?.status}`,
+          'error'
         )
       );
     }
@@ -39,22 +43,22 @@ function loadSupports() {
 
 function sendSupport(supportInfo, attachedFile) {
   return async (dispatch) => {
-    const token = getCookie("token");
+    const token = getCookie('token');
     const userId = parseJwt(token).unique_name;
 
     var bodyFormData = new FormData();
-    bodyFormData.append("userid", userId);
-    bodyFormData.append("subject", supportInfo?.subject);
-    bodyFormData.append("message", supportInfo?.message);
-    bodyFormData.append("contact", supportInfo?.contact);
-    bodyFormData.append("file", attachedFile);
+    bodyFormData.append('userid', userId);
+    bodyFormData.append('subject', supportInfo?.subject);
+    bodyFormData.append('message', supportInfo?.message);
+    bodyFormData.append('contact', supportInfo?.contact);
+    bodyFormData.append('file', attachedFile);
 
     axios({
-      method: "post",
+      method: 'post',
       url: `${process.env.REACT_APP_API_URL}Supports`,
       data: bodyFormData,
       headers: {
-        "Content-Type": "multipart/form-data",
+        'Content-Type': 'multipart/form-data',
         Authorization: `Bearer ${token}`,
       },
     })
@@ -63,7 +67,7 @@ function sendSupport(supportInfo, attachedFile) {
           type: SEND_SUPPORT_SUCCESS,
           payload: { support: response },
         });
-        dispatch(uiActions.setFlashMessage("ได้รับข้อมูลเรียบร้อย", "success"));
+        dispatch(uiActions.setFlashMessage('ได้รับข้อมูลเรียบร้อย', 'success'));
         window.location.reload();
       })
       .catch(function (err) {
@@ -71,21 +75,21 @@ function sendSupport(supportInfo, attachedFile) {
           dispatch(
             uiActions.setFlashMessage(
               `ไฟล์แนบมีขนาดใหญ่เกิน 20 MB โปรดเลือกไฟล์ใหม่อีกครั้ง`,
-              "error"
+              'error'
             )
           );
         } else if (err?.response?.status === 403) {
           dispatch(
             uiActions.setFlashMessage(
               `นามสกุลไฟล์ไม่รองรับ โปรดเลือกไฟล์ใหม่อีกครั้ง`,
-              "error"
+              'error'
             )
           );
         } else {
           dispatch(
             uiActions.setFlashMessage(
               `บันทึกข้อมูลไม่สำเร็จ เกิดข้อผิดพลาด ${err?.response?.status}`,
-              "error"
+              'error'
             )
           );
         }
@@ -95,7 +99,7 @@ function sendSupport(supportInfo, attachedFile) {
 
 function markSupportAsRead(supportId) {
   return async (dispatch) => {
-    const token = getCookie("token");
+    const token = getCookie('token');
     try {
       var { data } = await axios.patch(
         `/Supports/${supportId}`,
@@ -105,7 +109,7 @@ function markSupportAsRead(supportId) {
         {
           headers: {
             Authorization: `Bearer ${token}`,
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
         }
       );
@@ -113,12 +117,12 @@ function markSupportAsRead(supportId) {
         data = [];
       }
       window.location.reload();
-      dispatch(uiActions.setFlashMessage("บันทึกข้อมูลเรียบร้อย", "success"));
+      dispatch(uiActions.setFlashMessage('บันทึกข้อมูลเรียบร้อย', 'success'));
     } catch (err) {
       dispatch(
         uiActions.setFlashMessage(
           `บันทึกข้อมูลไม่สำเร็จ เกิดข้อผิดพลาด ${err?.response?.status}`,
-          "error"
+          'error'
         )
       );
     }
