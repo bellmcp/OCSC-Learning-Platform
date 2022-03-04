34b1bff9a0dae707295ff058cff7362f816fecdf
Wutipat Khamnuansin <bellmcp@gmail.com>
2022-03-04 13:16:47 +0700

    Fix get content url function not generate case sensitive value

--- src/utils/soureceUrl.ts
+++ src/utils/soureceUrl.ts
@@ -1,14 +1,9 @@
 export function generateContentSourceUrl(isMobile: boolean, url: string) {
-  const lowerCaseUrl = url?.toLowerCase();
-  if (lowerCaseUrl === null || lowerCaseUrl === undefined) {
-    return url;
+  if (isMobile) {
+    if (url?.includes('.mp4')) return url?.replace('.mp4', '-480.mp4')
+    else if (url?.includes('.MP4')) return url?.replace('.MP4', '-480.MP4')
+    else return url
   } else {
-    if (lowerCaseUrl.includes("youtube") || lowerCaseUrl.includes("youtu.be")) {
-      return url;
-    } else if (isMobile) {
-      return lowerCaseUrl?.replace(".mp4", "-480.mp4");
-    } else {
-      return lowerCaseUrl;
-    }
+    return url
   }
 }
