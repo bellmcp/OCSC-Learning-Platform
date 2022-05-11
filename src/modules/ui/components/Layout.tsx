df8682f05e3b15741391efd6c3a7e9c5ff32988d
Wutipat Khamnuansin <bellmcp@gmail.com>
2022-05-11 19:24:03 +0700

    Implement announcement modal

--- src/modules/ui/components/Layout.tsx
+++ src/modules/ui/components/Layout.tsx
@@ -1,9 +1,10 @@
 // @ts-nocheck
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useLocation, useHistory } from "react-router-dom";
-import LoadingBar from "react-redux-loading-bar";
-import useKonami from "use-konami";
+import React, { useState, useEffect } from 'react'
+import { get } from 'lodash'
+import { useSelector, useDispatch } from 'react-redux'
+import { useLocation, useHistory } from 'react-router-dom'
+import LoadingBar from 'react-redux-loading-bar'
+import useKonami from 'use-konami'
 import {
   CssBaseline,
   Snackbar,
@@ -17,28 +18,63 @@ import {
   Typography,
   Divider,
   Link,
-} from "@material-ui/core";
+  CircularProgress,
+} from '@material-ui/core'
 import {
   unstable_createMuiStrictModeTheme as createMuiTheme,
   ThemeProvider,
-} from "@material-ui/core/styles";
-import { Close as CloseIcon } from "@material-ui/icons";
-import Alert from "@material-ui/lab/Alert";
+} from '@material-ui/core/styles'
+import { Close as CloseIcon } from '@material-ui/icons'
+import Alert from '@material-ui/lab/Alert'
 
-import * as actions from "../actions";
-import NavBar from "./NavBar";
-import Routes from "./Routes";
-import Footer from "./Footer";
+import * as actions from '../actions'
+import NavBar from './NavBar'
+import Routes from './Routes'
+import Footer from './Footer'
+
+import { isLogin } from 'utils/isLogin'
+import * as pressesActions from 'modules/press/actions'
 
 export default function Layout() {
-  const { pathname } = useLocation();
-  const history = useHistory();
-  const PATH = process.env.REACT_APP_BASE_PATH;
-  const dispatch = useDispatch();
+  const { pathname } = useLocation()
+  const history = useHistory()
+  const PATH = process.env.REACT_APP_BASE_PATH
+  const dispatch = useDispatch()
   const { isSnackbarOpen, isDialogOpen, flashMessage, alertType } = useSelector(
     (state) => state.ui
-  );
-  const closeFlashMessage = () => dispatch(actions.clearFlashMessage());
+  )
+  const closeFlashMessage = () => dispatch(actions.clearFlashMessage())
+
+  const [announcementDialogOpen, setAnnouncementDialogOpen] = useState(false)
+
+  const { isAnnoucementLoading, announcement } = useSelector(
+    (state) => state.press
+  )
+
+  const handleAnnouncementDialogClose = () => {
+    setAnnouncementDialogOpen(false)
+  }
+
+  useEffect(() => {
+    const presses_action = pressesActions.loadAnnouncement()
+    dispatch(presses_action)
+  }, [dispatch])
+
+  useEffect(() => {
+    if (!isLogin()) {
+      if (isAnnoucementLoading) {
+        setAnnouncementDialogOpen(false)
+      } else {
+        if (get(announcement, 'mesg') !== '') {
+          setAnnouncementDialogOpen(true)
+        } else {
+          setAnnouncementDialogOpen(false)
+        }
+      }
+    } else {
+      setAnnouncementDialogOpen(false)
+    }
+  }, [announcement, isAnnoucementLoading])
 
   // //GET STATE FOR DEBUG
   // const loginState = useSelector((state) => state.login);
@@ -57,37 +93,37 @@ export default function Layout() {
     const setInitialActivePage = () => {
       switch (pathname) {
         case `${PATH}`:
-          setActivePage(0);
-          break;
+          setActivePage(0)
+          break
         case `${PATH}/learn`:
-          setActivePage(1);
-          break;
+          setActivePage(1)
+          break
         case `${PATH}/support`:
-          setActivePage(2);
-          break;
+          setActivePage(2)
+          break
         default:
-          setActivePage(99);
-          break;
+          setActivePage(99)
+          break
       }
-      if (pathname.includes("/learn/courses")) {
-        setActivePage(1);
+      if (pathname.includes('/learn/courses')) {
+        setActivePage(1)
       }
-    };
-    setInitialActivePage();
-  }, [pathname]);
+    }
+    setInitialActivePage()
+  }, [pathname])
 
-  const [activePage, setActivePage] = useState(0);
+  const [activePage, setActivePage] = useState(0)
 
-  const defaultTheme = createMuiTheme();
+  const defaultTheme = createMuiTheme()
 
   const theme = createMuiTheme({
     typography: {
-      fontFamily: ["Prompt", "sans-serif"].join(","),
+      fontFamily: ['Prompt', 'sans-serif'].join(','),
     },
     overrides: {
       MuiToolbar: {
         gutters: {
-          [defaultTheme.breakpoints.up("xs")]: {
+          [defaultTheme.breakpoints.up('xs')]: {
             paddingLeft: 0,
             paddingRight: 0,
           },
@@ -96,7 +132,7 @@ export default function Layout() {
       MuiCardContent: {
         root: {
           padding: 0,
-          "&:last-child": {
+          '&:last-child': {
             paddingBottom: 0,
           },
         },
@@ -118,27 +154,27 @@ export default function Layout() {
         main: process.env.REACT_APP_SECONDARY_COLOR_HEX,
       },
     },
-  });
+  })
 
   // DEBUG DIALOG
   useKonami({
     onUnlock: () => setDebugDialogOpen(true),
-  });
-  const [debugDialogOpen, setDebugDialogOpen] = useState(false);
+  })
+  const [debugDialogOpen, setDebugDialogOpen] = useState(false)
   const handleDebugDialogClose = () => {
-    setDebugDialogOpen(false);
-  };
+    setDebugDialogOpen(false)
+  }
 
   const handleDialogClose = () => {
-    dispatch(actions.setLearnExitDialog(false));
-  };
+    dispatch(actions.setLearnExitDialog(false))
+  }
   const linkToLearn = () => {
-    handleDialogClose();
-    history.push(`${PATH}/learn`);
+    handleDialogClose()
+    history.push(`${PATH}/learn`)
     dispatch(
-      actions.setFlashMessage("บันทึกเวลาเรียนสะสมเรียบร้อยแล้ว", "success")
-    );
-  };
+      actions.setFlashMessage('บันทึกเวลาเรียนสะสมเรียบร้อยแล้ว', 'success')
+    )
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -150,53 +186,97 @@ export default function Layout() {
           zIndex: 9999999999,
           height: 2,
           backgroundColor: theme.palette.primary.main,
-          transition: "all 5s ease 3s",
+          transition: 'all 5s ease 3s',
         }}
       />
       <NavBar active={activePage} setActivePage={setActivePage} />
       <Routes />
+      {/* ANNOUNCEMENT DIALOG */}
+      <Dialog
+        open={announcementDialogOpen}
+        onClose={handleAnnouncementDialogClose}
+      >
+        {isAnnoucementLoading ? (
+          <>
+            <CircularProgress />
+          </>
+        ) : (
+          <>
+            <DialogTitle style={{ margin: '16px 0' }}>
+              <Typography
+                variant='h5'
+                color='secondary'
+                align='center'
+                style={{ fontWeight: 600 }}
+              >
+                ประกาศ
+              </Typography>
+            </DialogTitle>
+            <DialogContent style={{ padding: '0 32px' }}>
+              <Typography variant='body1' align='center' color='textPrimary'>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: get(announcement, 'mesg', 'ไม่มีข้อความ'),
+                  }}
+                ></div>
+              </Typography>
+            </DialogContent>
+            <DialogActions style={{ margin: '16px 0', marginTop: 24 }}>
+              <div style={{ flex: '1 0 0' }} />
+              <Button
+                variant='contained'
+                color='secondary'
+                onClick={handleAnnouncementDialogClose}
+              >
+                รับทราบ
+              </Button>
+              <div style={{ flex: '1 0 0' }} />
+            </DialogActions>
+          </>
+        )}
+      </Dialog>
       {/* DEBUG DIALOG */}
       <Dialog open={debugDialogOpen} onClose={handleDebugDialogClose}>
         <DialogTitle onClose={handleDebugDialogClose}>เกี่ยวกับ</DialogTitle>
         <DialogContent dividers>
-          <Typography gutterBottom style={{ fontWeight: 600 }} variant="body1">
+          <Typography gutterBottom style={{ fontWeight: 600 }} variant='body1'>
             OCSC LEARNING SPACE (LEARNER PART)
             <br />
             Version 1.0.0
           </Typography>
-          <Divider style={{ margin: "16px 0" }} />
+          <Divider style={{ margin: '16px 0' }} />
           <Typography gutterBottom style={{ fontWeight: 600 }}>
             Developer
           </Typography>
-          <Typography gutterBottom variant="body2">
+          <Typography gutterBottom variant='body2'>
             WUTIPAT KHAMNUANSIN (Front-End)
             <br />
             ANUNYA PRASONGKIAT (Back-End)
           </Typography>
-          <Divider style={{ margin: "16px 0" }} />
+          <Divider style={{ margin: '16px 0' }} />
           <Typography gutterBottom style={{ fontWeight: 600 }}>
             Advisor
           </Typography>
-          <Typography gutterBottom variant="body2">
+          <Typography gutterBottom variant='body2'>
             ASSOC. PROF. CHATCHAWIT APORNTEWAN, Ph.D.
           </Typography>
-          <Divider style={{ margin: "16px 0" }} />
-          <Typography gutterBottom variant="caption" color="textSecondary">
-            Licensed under{" "}
+          <Divider style={{ margin: '16px 0' }} />
+          <Typography gutterBottom variant='caption' color='textSecondary'>
+            Licensed under{' '}
             <Link
-              href="https://github.com/bellmcp/OCSC-Learning-Platform/blob/master/LICENSE"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://github.com/bellmcp/OCSC-Learning-Platform/blob/master/LICENSE'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               The GNU General Public License v3.0 License
             </Link>
             .
             <br />
-            Copyright © {new Date().getFullYear()}{" "}
+            Copyright © {new Date().getFullYear()}{' '}
             <Link
-              href="https://www.ocsc.go.th/"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.ocsc.go.th/'
+              target='_blank'
+              rel='noopener noreferrer'
             >
               Office of the Civil Service Commission (OCSC)
             </Link>
@@ -216,23 +296,23 @@ export default function Layout() {
       <Dialog
         open={isDialogOpen}
         onClose={handleDialogClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+        aria-labelledby='alert-dialog-title'
+        aria-describedby='alert-dialog-description'
       >
-        <DialogTitle id="alert-dialog-title">{"ออกจากห้องเรียน?"}</DialogTitle>
+        <DialogTitle id='alert-dialog-title'>{'ออกจากห้องเรียน?'}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
+          <DialogContentText id='alert-dialog-description'>
             เซสชันปัจจุบันจะจบลง และเวลาเรียนสะสมของคุณจะถูกบันทึก
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleDialogClose} color="secondary">
+          <Button onClick={handleDialogClose} color='secondary'>
             ยกเลิก
           </Button>
           <Button
-            color="secondary"
+            color='secondary'
             autoFocus
-            variant="contained"
+            variant='contained'
             disableElevation
             onClick={linkToLearn}
           >
@@ -247,30 +327,30 @@ export default function Layout() {
         autoHideDuration={6000}
         action={
           <IconButton
-            size="small"
-            aria-label="close"
-            color="inherit"
+            size='small'
+            aria-label='close'
+            color='inherit'
             onClick={closeFlashMessage}
           >
-            <CloseIcon fontSize="small" />
+            <CloseIcon fontSize='small' />
           </IconButton>
         }
         style={{
           marginBottom: pathname.includes(`${PATH}/learn/courses`)
             ? 60
-            : "unset",
+            : 'unset',
         }}
       >
         <Alert
           onClose={closeFlashMessage}
-          severity={alertType ? alertType : "info"}
+          severity={alertType ? alertType : 'info'}
           elevation={6}
-          variant="filled"
+          variant='filled'
         >
           {flashMessage}
         </Alert>
       </Snackbar>
       {!pathname.includes(`${PATH}/learn/courses`) && <Footer />}
     </ThemeProvider>
-  );
+  )
 }
