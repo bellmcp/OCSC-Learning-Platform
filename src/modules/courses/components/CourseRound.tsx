1f6b8eaa2a9f7260a193619d482db211bd907a89
Wutipat Khamnuansin <bellmcp@gmail.com>
2022-07-22 17:40:44 +0700

    Course detail support 100 million students

--- src/modules/courses/components/CourseRound.tsx
+++ src/modules/courses/components/CourseRound.tsx
@@ -1,36 +1,36 @@
 // @ts-nocheck
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import DayJS from 'react-dayjs';
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import DayJS from 'react-dayjs'
 import {
   Typography,
   Box,
   Grid,
   LinearProgress,
   Button,
-} from '@material-ui/core';
-import { ArrowForwardIos as ArrowForwardIcon } from '@material-ui/icons';
-import { isLogin } from 'utils/isLogin';
-import isBetween from 'utils/isBetween';
+} from '@material-ui/core'
+import { ArrowForwardIos as ArrowForwardIcon } from '@material-ui/icons'
+import { isLogin } from 'utils/isLogin'
+import isBetween from 'utils/isBetween'
 
-import * as registrationsActions from 'modules/registrations/actions';
+import * as registrationsActions from 'modules/registrations/actions'
 
-import { CourseProps } from '../types';
+import { CourseProps } from '../types'
 
 interface CourseRoundProps {
-  id: number;
-  name: string;
-  registrationStart: string;
-  registrationEnd: string;
-  registrationCondition: string;
-  courseStart: string;
-  courseEnd: string;
-  maxStudents: number;
-  numStudents: number;
-  myCourses: CourseProps[];
-  courseId: number;
-  localDateTime: string[];
+  id: number
+  name: string
+  registrationStart: string
+  registrationEnd: string
+  registrationCondition: string
+  courseStart: string
+  courseEnd: string
+  maxStudents: number
+  numStudents: number
+  myCourses: CourseProps[]
+  courseId: number
+  localDateTime: string[]
 }
 
 export default function CourseRound({
@@ -47,58 +47,58 @@ export default function CourseRound({
   courseId,
   localDateTime,
 }: CourseRoundProps) {
-  const dispatch = useDispatch();
-  const history = useHistory();
-  const PATH = process.env.REACT_APP_BASE_PATH;
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const PATH = process.env.REACT_APP_BASE_PATH
   const [isRegisterButtonDisabled, setIsRegisterButtonDisabled] =
-    useState(false);
+    useState(false)
   const [registerButtonLabel, setRegisterButtonLabel] =
-    useState('ลงทะเบียนเรียน');
+    useState('ลงทะเบียนเรียน')
 
   const isEligibleForAccess = isBetween(
     registrationStart,
     registrationEnd,
     localDateTime
-  );
+  )
 
   if (myCourses === '') {
-    myCourses = [];
+    myCourses = []
   }
 
   const linkToLogin = () => {
-    history.push(`${PATH}/login`);
-  };
+    history.push(`${PATH}/login`)
+  }
 
   const linkToLearn = () => {
-    history.push(`${PATH}/learn/courses/${courseId}`);
-  };
+    history.push(`${PATH}/learn/courses/${courseId}`)
+  }
 
   const registerCourse = () => {
-    const registration_action = registrationsActions.registerCourse(id);
-    dispatch(registration_action);
-    setIsRegisterButtonDisabled(true);
-    setRegisterButtonLabel('กำลังโหลด...');
+    const registration_action = registrationsActions.registerCourse(id)
+    dispatch(registration_action)
+    setIsRegisterButtonDisabled(true)
+    setRegisterButtonLabel('กำลังโหลด...')
     //PREVENT MULTIPLE REQUEST
     setTimeout(() => {
-      setIsRegisterButtonDisabled(false);
-      setRegisterButtonLabel('ลงทะเบียนเรียน');
-    }, 3000);
-  };
+      setIsRegisterButtonDisabled(false)
+      setRegisterButtonLabel('ลงทะเบียนเรียน')
+    }, 3000)
+  }
 
   function renderRegisterButton() {
     if (!isLogin()) {
       return (
         <Grid item>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant='body2' color='textSecondary'>
             โปรดเข้าสู่ระบบเพื่อลงทะเบียนรายวิชา
           </Typography>
           <Box my={1}>
-            <Button color="secondary" variant="contained" onClick={linkToLogin}>
+            <Button color='secondary' variant='contained' onClick={linkToLogin}>
               เข้าสู่ระบบ
             </Button>
           </Box>
         </Grid>
-      );
+      )
     } else if (
       isLogin() &&
       myCourses.filter(
@@ -109,13 +109,13 @@ export default function CourseRound({
     ) {
       return (
         <Grid item>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant='body2' color='textSecondary'>
             คุณลงทะเบียนรอบนี้แล้ว เข้าเรียนได้เลย
           </Typography>
           <Box my={2}>
             <Button
-              color="secondary"
-              variant="contained"
+              color='secondary'
+              variant='contained'
               endIcon={<ArrowForwardIcon />}
               onClick={linkToLearn}
             >
@@ -123,36 +123,163 @@ export default function CourseRound({
             </Button>
           </Box>
         </Grid>
-      );
+      )
     } else if (numStudents >= maxStudents) {
       return (
         <Grid item>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant='body2' color='textSecondary'>
             จำนวนผู้เรียนเต็มแล้ว
           </Typography>
         </Grid>
-      );
+      )
     } else if (!isEligibleForAccess) {
       return (
         <Grid item>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant='body2' color='textSecondary'>
             ไม่เปิดให้ลงทะเบียน
           </Typography>
         </Grid>
-      );
+      )
     } else {
       return (
         <Button
-          variant="contained"
-          color="primary"
+          variant='contained'
+          color='primary'
           endIcon={<ArrowForwardIcon />}
           onClick={registerCourse}
           disabled={isRegisterButtonDisabled}
         >
           {registerButtonLabel}
         </Button>
-      );
+      )
     }
+  }
+
+  if (maxStudents >= 100_000_000) {
+    return (
+      <Grid container spacing={6}>
+        <Grid item xs={12} sm={7}>
+          <Typography
+            style={{
+              fontSize: '1.7rem',
+              lineHeight: '1.2',
+              fontWeight: 600,
+            }}
+            gutterBottom
+          >
+            {name}
+          </Typography>
+          <Box mb={3}>
+            <Box display='flex' alignItems='center'>
+              <Box width='100%'>
+                <Typography variant='body1' color='primary'>
+                  <span style={{ fontWeight: 600 }}>
+                    {numStudents.toLocaleString()} คน
+                  </span>{' '}
+                  ลงทะเบียนเรียนรอบนี้แล้ว
+                </Typography>
+              </Box>
+              <Box></Box>
+            </Box>
+          </Box>
+          <Box my={3}>{renderRegisterButton()}</Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sm={5}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <Grid
+            container
+            spacing={3}
+            alignItems='baseline'
+            style={{ marginBottom: 4 }}
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant='h6'
+                style={{ lineHeight: '1.2' }}
+                gutterBottom
+              >
+                เปิดให้ลงทะเบียน
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant='body2' color='textSecondary'>
+                <DayJS format='D/M/YYYY' add={{ years: 543 }}>
+                  {registrationStart}
+                </DayJS>{' '}
+                ถึง{' '}
+                <DayJS format='D/M/YYYY' add={{ years: 543 }}>
+                  {registrationEnd}
+                </DayJS>
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={3}
+            alignItems='baseline'
+            style={{ marginBottom: 4 }}
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant='h6'
+                style={{ lineHeight: '1.2' }}
+                gutterBottom
+              >
+                เงื่อนไขการลงทะเบียน
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant='body2' color='textSecondary'>
+                {registrationCondition ? (
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: registrationCondition,
+                    }}
+                  ></div>
+                ) : (
+                  'ไม่มีเงื่อนไข'
+                )}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            spacing={3}
+            alignItems='baseline'
+            style={{ marginBottom: 4 }}
+          >
+            <Grid item xs={6}>
+              <Typography
+                variant='h6'
+                style={{ lineHeight: '1.2' }}
+                gutterBottom
+              >
+                เข้าเรียนได้
+              </Typography>
+            </Grid>
+            <Grid item xs={6}>
+              <Typography variant='body2' color='textSecondary'>
+                <DayJS format='D/M/YYYY' add={{ years: 543 }}>
+                  {courseStart}
+                </DayJS>{' '}
+                ถึง{' '}
+                <DayJS format='D/M/YYYY' add={{ years: 543 }}>
+                  {courseEnd}
+                </DayJS>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
   }
 
   return (
@@ -169,16 +296,16 @@ export default function CourseRound({
           {name}
         </Typography>
         <Box mb={3}>
-          <Box display="flex" alignItems="center">
-            <Box width="100%">
-              <Typography variant="body2" color="textSecondary" align="right">
+          <Box display='flex' alignItems='center'>
+            <Box width='100%'>
+              <Typography variant='body2' color='textSecondary' align='right'>
                 {numStudents.toLocaleString()} / {maxStudents.toLocaleString()}{' '}
                 คน
               </Typography>
               <LinearProgress
-                variant="determinate"
+                variant='determinate'
                 value={(numStudents / maxStudents) * 100}
-                color="primary"
+                color='primary'
               />
             </Box>
             <Box></Box>
@@ -187,32 +314,32 @@ export default function CourseRound({
         <Box my={3}>{renderRegisterButton()}</Box>
       </Grid>
       <Grid item xs={12} sm={5}>
-        <Grid container spacing={3} alignItems="baseline">
+        <Grid container spacing={3} alignItems='baseline'>
           <Grid item xs={6}>
-            <Typography variant="h6" style={{ lineHeight: '1.2' }} gutterBottom>
+            <Typography variant='h6' style={{ lineHeight: '1.2' }} gutterBottom>
               เปิดให้ลงทะเบียน
             </Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body2" color="textSecondary">
-              <DayJS format="D/M/YYYY" add={{ years: 543 }}>
+            <Typography variant='body2' color='textSecondary'>
+              <DayJS format='D/M/YYYY' add={{ years: 543 }}>
                 {registrationStart}
               </DayJS>{' '}
               ถึง{' '}
-              <DayJS format="D/M/YYYY" add={{ years: 543 }}>
+              <DayJS format='D/M/YYYY' add={{ years: 543 }}>
                 {registrationEnd}
               </DayJS>
             </Typography>
           </Grid>
         </Grid>
-        <Grid container spacing={3} alignItems="baseline">
+        <Grid container spacing={3} alignItems='baseline'>
           <Grid item xs={6}>
-            <Typography variant="h6" style={{ lineHeight: '1.2' }} gutterBottom>
+            <Typography variant='h6' style={{ lineHeight: '1.2' }} gutterBottom>
               เงื่อนไขการลงทะเบียน
             </Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body2" color="textSecondary">
+            <Typography variant='body2' color='textSecondary'>
               {registrationCondition ? (
                 <div
                   dangerouslySetInnerHTML={{
@@ -225,37 +352,37 @@ export default function CourseRound({
             </Typography>
           </Grid>
         </Grid>
-        <Grid container spacing={3} alignItems="baseline">
+        <Grid container spacing={3} alignItems='baseline'>
           <Grid item xs={6}>
-            <Typography variant="h6" style={{ lineHeight: '1.2' }} gutterBottom>
+            <Typography variant='h6' style={{ lineHeight: '1.2' }} gutterBottom>
               เข้าเรียนได้
             </Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body2" color="textSecondary">
-              <DayJS format="D/M/YYYY" add={{ years: 543 }}>
+            <Typography variant='body2' color='textSecondary'>
+              <DayJS format='D/M/YYYY' add={{ years: 543 }}>
                 {courseStart}
               </DayJS>{' '}
               ถึง{' '}
-              <DayJS format="D/M/YYYY" add={{ years: 543 }}>
+              <DayJS format='D/M/YYYY' add={{ years: 543 }}>
                 {courseEnd}
               </DayJS>
             </Typography>
           </Grid>
         </Grid>
-        <Grid container spacing={3} alignItems="baseline">
+        <Grid container spacing={3} alignItems='baseline'>
           <Grid item xs={6}>
-            <Typography variant="h6" style={{ lineHeight: '1.2' }} gutterBottom>
+            <Typography variant='h6' style={{ lineHeight: '1.2' }} gutterBottom>
               จำนวนผู้เรียนสูงสุด
             </Typography>
           </Grid>
           <Grid item xs={6}>
-            <Typography variant="body2" color="textSecondary">
+            <Typography variant='body2' color='textSecondary'>
               {maxStudents.toLocaleString()} คน
             </Typography>
           </Grid>
         </Grid>
       </Grid>
     </Grid>
-  );
+  )
 }
